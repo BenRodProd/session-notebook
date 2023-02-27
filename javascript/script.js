75deef1e3eeebe7c8a38f9d96f84770e465dc7f0
Benjamin Rodenstein <benrodprod@gmail.com>
2023-02-27 15:27:36 +0100

    css module and homepage tweak

---
+++ javascript/script.js
@@ -0,0 +1,34 @@
+// PACMAN MIT MAUS STEUERN
+// document.addEventListener("mousemove", function (e) {
+//   let circle = document.getElementById("circle");
+//   let left = e.layerX;
+//   let top = e.layerY;
+//   circle.style.left = left + "px";
+//   circle.style.top = top + "px";
+// });
+// var projectwidth = document.getElementById("projectlist").offsetWidth;
+// function moveScanner() {
+//   var interval = setInterval(function () {
+//     var div = document.getElementById("scanner");
+//     div.style.left = div.offsetLeft + 1 + "px";
+//   }, projectwidth);
+//   var interval = setInterval(function () {
+//     var div = document.getElementById("scanner");
+//     div.style.left = div.offsetLeft - 1 + "x";
+//   }, projectwidth);
+//   //setTimeout(moveScanner(), 2000);
+// }
+// moveScanner();
+const jf = document.getElementById("jugendforscht");
+console.log(jf);
+// jugendforscht.addEventListener("mouseover", () => {
+//   console.log("yea");
+//   const jfimg = document.createElement("img");
+//   const jfimgdiv = document.createElement("div");
+//   jfimgdiv.setAttribute("id", "jfimgdiv");
+
+//   jfimg.setAttribute("id", "jfimg");
+//   jfimg.scr = "../html/jugendforscht95jpg.jpg";
+//   jfimg.appendChild();
+//   jfimgdiv.appendChild();
+// });
